53a2522e9125649a1a5463e9507e0ce2377f15b1
Aaditya8C <aadityajp0419@gmail.com>
2024-03-22 14:24:53 +0530

    contract_setup_done

--- contracts/SocialMedia.sol
+++ contracts/SocialMedia.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.2;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Lock {
+contract SocialMedia {
     uint public unlockTime;
     address payable public owner;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_API_KEY,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  paths: {
+    artifacts: "../client/app/artifacts",
+  },
 };
